8cb036c8ee194642b0cd8d45df9326d8c9f495d4
ALIFAHMEDONGKON21 <mdalifahmed114510@gmail.com>
2024-08-31 00:12:14 +0600

    some tarnary

--- tarnary.js
+++ tarnary.js
@@ -1,4 +1,47 @@
 // condition ? ture : false
-var age=19;
+// var agee=19;
 
-age>=19 ? console.log("YOU ARE VOTER") : console.log("YOU ARE NOT VOTER")
+// age>=19 ? console.log("YOU ARE VOTER") : console.log("YOU ARE NOT VOTER")
+
+ 
+// let leader =false;
+// var price = leader ===false ? 0 : price+200;
+
+// if(leader === true)
+// {
+//     price=0;
+// }
+// else
+// {
+//     price=price+100;
+// }
+
+// console.log(price)
+
+
+// let age=10;
+
+// // if(age>=18)
+// {
+//     console.log("adul");
+// }
+// else
+// {
+//     console.log("not  adul");
+// }
+
+
+// // age>=18 ? "adul" : "not adul"
+
+let price = 500;
+const isLeader = false;
+
+if(isLeader === true){
+    price = 0;
+}
+else {
+    price = price + 100;
+}
+// console.log(price)
+
+price = isLeader === true ? 0 : price + 100;
