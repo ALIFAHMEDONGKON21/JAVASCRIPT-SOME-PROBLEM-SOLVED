b24d63cdda4f522cc8e1269c22c891328d294e5c
ALIFAHMEDONGKON21 <mdalifahmed114510@gmail.com>
2024-09-04 22:42:09 +0600

    some prblem sloved

---
+++ arrayloopingT.js
@@ -0,0 +1,9 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+const newcolor=[];
+for(let i=colors.length-1; i>0; i--)
+{
+   newcolor.push(colors[i]);
+    
+}
+console.log(newcolor);
---
+++ mod.js
@@ -0,0 +1,37 @@
+// var name  ="alif";
+
+// let x =10;
+// console.log(x);
+
+// const y=30.5;
+// console.log(y);
+
+
+// console.log(typeof(y));
+//  console.log(typeof(name));
+
+// let a=10;
+// let b=20;
+
+// let c=a+b;
+
+let youpass =true;
+
+
+
+console.log(typeof(youpass));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
