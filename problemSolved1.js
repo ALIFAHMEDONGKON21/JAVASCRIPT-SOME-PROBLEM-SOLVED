748482f27f1303049a9be644ab3795e7c293eaf9
ALIFAHMEDONGKON21 <mdalifahmed114510@gmail.com>
2024-09-07 23:11:47 +0600

    prblem sloved

---
+++ problemSolved1.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Example usage:
+let tempCelsius = 25;
+let tempFahrenheit = celsiusToFahrenheit(tempCelsius);
+console.log(`${tempCelsius}°C is ${tempFahrenheit}°F`);
