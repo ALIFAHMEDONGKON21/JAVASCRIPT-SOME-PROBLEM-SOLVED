30adf85ea438390598c7c9ba1bb730e883b9bcb5
ALIFAHMEDONGKON21 <mdalifahmed114510@gmail.com>
2024-09-13 04:21:10 +0600

    some problem sloved

--- sum.js
+++ sum.js
@@ -1,11 +1,20 @@
-let sum=0;
+let arr =[20, 40, 55, 45,65,60,66,77,744,3443];
 
-
-for(let i=15; i<=30; i++)
+for(let i=1; i<=arr.length; i++)
 {
-    if(i%2==0)
-    {
-       sum=sum+i;
-    }
+    console.log(arr[i]);
 }
-console.log(sum);
+
+let x=[55, 66, 78,65, 35,70 ,95, 80];
+
+for(let number of x)
+{
+    console.log(number);
+}
+
+let y=["addkfk", "Dfdas","FDADFDA","DFDADFA"];
+
+for(let name of y)
+{
+    console.log(name);
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-console.log(arr[i]);
+
+{
+    console.log(number);
+}
